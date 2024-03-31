9b2967079ed83d56a75b0d49de0bce85cd25000c
Alyssia Fourali <alyssiafourali@Alyssias-MacBook-Air.local>
2024-03-31 10:48:21 +0200

    fusion des 2 classes BagOfPrimitivesDeserializationBenchmark et CollectionsDeserializationBenchmark en une seule classe DeserializationBenchmark

--- gson/src/main/java/com/google/gson/JsonArray.java
+++ gson/src/main/java/com/google/gson/JsonArray.java
@@ -223,7 +223,7 @@ public final class JsonArray extends JsonElement implements Iterable<JsonElement
     if (size == 1) {
       return elements.get(0);
     }
-    throw new IllegalStateException("Array must have size 1, but has size " + size);
+    throw new IllegalStatcleException("Array must have size 1, but has size " + size);
   }
 
   /**
